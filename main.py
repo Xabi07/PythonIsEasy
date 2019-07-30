6c9e79677620ae10f95694d333e23616011b09d8
Xabi07 <40674978+Xabi07@users.noreply.github.com>
2019-07-30 12:42:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+Author MyName
+Date July 2019
+Course Pyhton is Easy on Pirple
+Assignment 1: Variables
+"""
+
+import datetime #date library
+
+
+title = "Enter Sandman"
+band = "Metallica"
+duration = datetime.time(0,5,30)
+releasedate = datetime.date(1991, 6,15)
+genre = "Metal"
+album = "Metallica"
+price = 10.5
+durationInSeconds = 330
+country = "USA"
+members = 4
+leadvocals = "James Hetfield"
+drummer = "Lars Ulrich"
+leadGuitar = "Guitar Man"
+
+
+print("Title: ", title)
+print("Band: " , band)
+print("Country of Origin: ", country)
+print("Song Duration: " , duration)
+print("Release Date: " , releasedate)
+print("Genre: " , genre)
+print("Album: " , album)
+print("Album Cost: $",price)
+print("Song duration in seconds: ", durationInSeconds)
+print("No of Members in Band: ", members)
+print("Lead Vocals: ", leadvocals)
+print("Lead Guitar: ", leadGuitar)
+print("Drummer: " ,drummer)
